c0cdb5dc7605cf9ff08e0d3cfa8a4fa13145ba65
Badnam143 <154507591+Badnam143@users.noreply.github.com>
2023-12-31 18:09:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,8 +3,8 @@ class Config:
     API_ID=24509589
     API_HASH="717cf21d94c4934bcbe1eaa1ad86ae75"
     TOKEN="6796545941:AAGyuhnW9Gi4nCbyUsrB-SzbqUHArgtHVXg"
-    SUDO = list(int(i) for i in os.environ.get("SUDO", "6691393517").split(" "))
-    START_IMG="https://telegra.ph/file/52fefb8bd51289a83a49b.jpg"
+    SUDO = list(int(i) for i in os.environ.get("SUDO", "5674525908").split(" "))
+    START_IMG="https://graph.org/file/c78c7879bac12a90a4137.jpg"
     BOT_ID=6796545941
-    BOT_USERNAME="ZuliaiBot"
-    BOT_NAME="ZuliaiBot"
+    BOT_USERNAME="Badnam_banall_bot"
+    BOT_NAME="badnam"
